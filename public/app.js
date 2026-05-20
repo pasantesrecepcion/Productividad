78fe8d372aceae3ca973e8b2beb9fdd60764b73d
pasantesrecepcion <pasantesrecepcion@gmail.com>
2026-05-20 15:00:44 -0400

    15 vercel
    modificacion de la llave.

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// URL Empresarial Ocupando la Raíz
 const DATABASE_URL = 'https://logistica-b100-default-rtdb.firebaseio.com/.json';
 
 async function getFirebaseData() {
@@ -15,41 +14,49 @@ async function getFirebaseData() {
 }
 
 const server = http.createServer(async (req, res) => {
+
     if (req.url === '/' || req.url === '/index.html') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
+
     } else if (req.url === '/style.css') {
         fs.readFile(path.join(__dirname, 'style.css'), (err, data) => {
             res.writeHead(200, { 'Content-Type': 'text/css' });
             res.end(data);
         });
+
     } else if (req.url === '/dashboard.js') {
         fs.readFile(path.join(__dirname, 'dashboard.js'), (err, data) => {
             res.writeHead(200, { 'Content-Type': 'application/javascript' });
             res.end(data);
         });
-     } else if (req.url === '/api/data') {
-        // Agregar preflight OPTIONS
-          if (req.method === 'OPTIONS') {
-          res.writeHead(204, {
+
+    } else if (req.url === '/api/data') {
+        if (req.method === 'OPTIONS') {
+            res.writeHead(204, {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            });
+            res.end();
+            return;
+        }
+        const data = await getFirebaseData();
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type'
-         });
-         res.end();
-         return;
-          }
-    const data = await getFirebaseData();
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',          // ← ESTO resuelve Vercel
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
-    });
-    res.end(JSON.stringify(data));
-}
+        });
+        res.end(JSON.stringify(data));
+
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
